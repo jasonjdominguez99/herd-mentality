ca1ba957c13137777cc8912127ceb8cc81f111a3
Jason Dominguez <jasonjdominguez99@gmail.com>
2022-01-23 18:29:25 +0000

    Added default values to Player class

--- api/models/models.py
+++ api/models/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 import flask_sqlalchemy
 
 db = flask_sqlalchemy.SQLAlchemy()
@@ -5,9 +6,9 @@ db = flask_sqlalchemy.SQLAlchemy()
 class Player(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
-    score = db.Column(db.Integer)
-    has_cow = db.Column(db.Boolean)
-    answer = db.Column(db.String(100))
+    score = db.Column(db.Integer, default=0)
+    has_cow = db.Column(db.Boolean, default=False)
+    answer = db.Column(db.String(100), default="")
 
 class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True)
