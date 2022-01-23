1fef5bb9815ff271896a1fa79a48e8d5daf17560
Jason Dominguez <jasonjdominguez99@gmail.com>
2022-01-23 15:54:21 +0000

    Created Player class and added ID to player query

---
+++ api/models/player.py
@@ -0,0 +1,10 @@
+import flask_sqlalchemy
+
+db = flask_sqlalchemy.SQLAlchemy()
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    score = db.Column(db.Integer)
+    has_cow = db.Column(db.Boolean)
+    answer = db.Column(db.String(100))
