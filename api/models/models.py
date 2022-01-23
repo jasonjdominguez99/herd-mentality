080e7103d648837cad4f00456cd984598b216088
Jason Dominguez <jasonjdominguez99@gmail.com>
2022-01-23 16:06:53 +0000

    Created Card class

--- api/models/models.py
+++ api/models/models.py
@@ -8,3 +8,7 @@ class Player(db.Model):
     score = db.Column(db.Integer)
     has_cow = db.Column(db.Boolean)
     answer = db.Column(db.String(100))
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String(255))
