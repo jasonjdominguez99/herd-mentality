ae878f217ebc20cfebdf2f76f46ec38ef05aa793
Jason Dominguez <jasonjdominguez99@gmail.com>
2022-01-24 22:03:21 +0000

    created constructors for player and card model classes

--- api/models/models.py
+++ api/models/models.py
@@ -10,6 +10,14 @@ class Player(db.Model):
     has_cow = db.Column(db.Boolean, default=False)
     answer = db.Column(db.String(100), default="")
 
+    def __init__(self, id, name):
+        super().__init__()
+        self.id = id
+        self.name = name
+        self.score = 0
+        self.has_cow = False
+        self.answer = ""
+
     def to_dict(self):
         return {
             "id": self.id,
@@ -23,6 +31,12 @@ class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.String(255))
     has_been_picked = db.column(db.Boolean, default=False)
+
+    def __init__(self, id, question):
+        super().__init__()
+        self.id = id
+        self.question = question
+        self.has_been_picked = False
 
     def to_dict(self):
         return {
