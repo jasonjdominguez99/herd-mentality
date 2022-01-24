0e1f370e51983caa3e65358e779facb8ccd34e2b
Jason Dominguez <jasonjdominguez99@gmail.com>
2022-01-24 21:36:11 +0000

    created card and player query resolvers

---
+++ api/resolvers/card_query.py
@@ -0,0 +1,12 @@
+from ..models.models import Card
+import random
+
+def resolve_card(obj, info):
+    available_card_ids = [card.id for card
+                          in Card.query.filter_by(has_been_picked=False).all()]
+
+    random.seed()
+    rand_idx = random.randint(0, len(available_card_ids) - 1)
+    rand_card_id = available_card_ids[rand_idx]
+
+    return Card.query.filter_by(id=rand_card_id)
--- api/resolvers/player_query.py
+++ api/resolvers/player_query.py
@@ -1,4 +1,4 @@
 from ..models.models import Player
 
 def resolve_players(obj, info):
-    players = []
+    return [player.to_dict() for player in Player.query.all()]
