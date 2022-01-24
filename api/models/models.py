c6753f255eaaae0abff406e2a0b3c7de030cadd4
Jason Dominguez <jasonjdominguez99@gmail.com>
2022-01-24 20:18:00 +0000

    Added to_dict methods to Player and Card classes in models

--- api/models/models.py
+++ api/models/models.py
@@ -10,6 +10,21 @@ class Player(db.Model):
     has_cow = db.Column(db.Boolean, default=False)
     answer = db.Column(db.String(100), default="")
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "score": self.score,
+            "has_cow": self.has_cow,
+            "answer": self.answer
+        }
+
 class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.String(255))
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "question": self.question
+        }
