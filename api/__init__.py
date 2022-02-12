0ffa3bff96be5757fd10a0ca960905af7aa60c00
Jason Dominguez <jasonjdominguez99@gmail.com>
2022-02-12 18:38:30 +0000

    fixed misc attribute errors and install missing dependencies and added to requirements.txt

--- api/__init__.py
+++ api/__init__.py
@@ -2,12 +2,12 @@ from flask import Flask
 import flask_sqlalchemy
 
 from models.models import db
-import config.config
+from config.config import DATABASE_CONNECTION_URI
 
 
 def created_app():
     flask_app = Flask(__name__)
-    flask_app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_CONNECTION_URI
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_CONNECTION_URI
     flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     flask_app.app_context().push()
     db.init_app(flask_app)
