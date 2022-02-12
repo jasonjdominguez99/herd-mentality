27e9ad2759fa8a4743c634c1c2e4f15ef276417f
Jason Dominguez <jasonjdominguez99@gmail.com>
2022-02-12 18:38:30 +0000

    fixed misc attribute errors and install missing dependencies and added to requirements.txt

--- api/config/config.py
+++ api/config/config.py
@@ -9,7 +9,7 @@ import os
 user = "Jason"
 password = "purple-cow"
 host = "postgres"
-database = "5432"
-port = "herd-mentality"
+database = "herd-mentality"
+port = "5432"
 
 DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
